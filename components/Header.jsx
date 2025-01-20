84d29e711e9f65b4bfa99c0219b90f32c8902652
czDamian <olebueziedamian@gmail.com>
2025-01-20 20:27:19 +0100

    fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="fixed text-white top-5 left-0 right-0 mx-auto text-sm max-w-[90vw] z-50">
-      <div className="flex justify-between items-center gap-8  px-6 py-3 rounded-full bg-opacity-50 backdrop-blur-[900px] z-50">
+      <div className="flex justify-between items-center gap-8 px-8 sm:px-16 py-3 rounded-full bg-opacity-50 backdrop-blur-[900px] z-50">
         <div className="flex-shrink-0">
           <Image
             src="/assets/gidaLogo.svg"
@@ -28,12 +28,15 @@ const Header = () => {
           <NavLink href="/">Blog</NavLink>
         </nav>
         <div className="flex gap-2 uppercase">
-          <button className="bg-gradient-to-r from-[#F7C338] hover:from-[#dfb02d] to-[#FF4B59] hover:to-[#ee3f4d] text-white rounded-full uppercase font-bold px-4 py-2 hidden md:block">
+          <Link
+            href="/chainup"
+            className="bg-gradient-to-r from-[#F7C338] hover:from-[#dfb02d] to-[#FF4B59] hover:to-[#ee3f4d] text-white rounded-full uppercase font-bold px-4 py-1.5 sm:py-2"
+          >
             chain up
-          </button>
+          </Link>
           <Link
             href="/register"
-            className="bg-[#900A15] hover:bg-[#a20b17] uppercase px-4 py-2 rounded-full"
+            className="bg-[#900A15] hover:bg-[#a20b17] uppercase px-4 py-1.5 sm:py-2 rounded-full"
           >
             register
           </Link>
