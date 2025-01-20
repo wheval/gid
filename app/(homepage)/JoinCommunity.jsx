583d13be19d4ac2f15a7ad1eeba58d0c7291f0d6
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-01-20 22:33:54 +0100

    Updated files

--- app/(homepage)/JoinCommunity.jsx
+++ app/(homepage)/JoinCommunity.jsx
@@ -5,7 +5,7 @@ const JoinCommunity = () => {
     <div className="mt-8 pl-2">
       <h1 className="text-6xl font-medium mb-4">Join Our Vibrant Community</h1>
       <div>
-        <p className="text-2xl">We have a ton of communities tailored in making people gain mastery in various digital and</p>
+        <p className="text-2xl">We have a ton of communities tailored in making people gain mastery in various digital skills and</p>
         <p className="text-2xl">fields of Web3</p>
       </div>
       
--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import WhyWeDifferent from '../(homepage)/WhyWeDifferent'
 import JoinCommunity from '../(homepage)/JoinCommunity'
 import GidaDeveloperComSection from '../(homepage)/GidaDeveloperComSection'
+import HeroSection from '../(homepage)/HeroSection'
+
 
 const HomePage = () => {
   return (
     <div>
+      <HeroSection/>
       <WhyWeDifferent/>
       <JoinCommunity/>
       <GidaDeveloperComSection/>
