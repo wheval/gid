9b5db7aec463d9ff70045b80434c687063f8c823
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-01-20 11:25:01 +0100

    Added JoinCommunitySection

--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WhyWeDifferent from '../(homepage)/WhyWeDifferent'
+import JoinCommunity from '../(homepage)/JoinCommunity'
 
 const HomePage = () => {
   return (
     <div>
       <WhyWeDifferent/>
+      <JoinCommunity/>
     </div>
   )
 }
