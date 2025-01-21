d39db36498f649eba84ffe7b229490b170940dff
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-01-21 10:05:17 +0100

    Added responsiveness to activities section

--- app/(homepage)/Activities.jsx
+++ app/(homepage)/Activities.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Activities = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center md:items-start gap-8 p-16">
+    <div className="flex flex-col lg:flex-row items-center md:items-start gap-8 p-16">
       {/* Text Content */}
       <div className="flex items-center h-screen md:w-1/2">
         <div className=''>
-          <h1 className="text-5xl font-semibold mb-8 pr-10">
+          <h1 className="text-5xl font-semibold mb-8 md:pr-10">
             Some Activities from our Developer Community &gt;&gt;
           </h1>
           <a
@@ -21,7 +21,7 @@ const Activities = () => {
 
       {/* Image Section */}
       <div className=" h-screen w-full md:w-1/2 relative">
-        <img src="/assets/leftarrow.png" className='absolute w-14 -left-8 top-1/3 z-50' alt="" />
+        <img src="/assets/leftarrow.png" className='absolute w-14 -left-8 top-1/3 z-10' alt="" />
         {/* First Image */}
         <div className="absolute w-80 h-80  left-0 top-20">
           <img
@@ -31,14 +31,14 @@ const Activities = () => {
           />
         </div>
         {/* Second Image */}
-        <div className="absolute w-96 h-96 left-56 top-16 z-20">
+        <div className="absolute w-96 h-96 left-56 top-16 ">
           <img
             src="/assets/activities2.png"
             className="w-full transform "
             alt="Activities 2"
           />
         </div>
-        <img src="/assets/rightarrow.png" className='absolute w-14 -right-1 top-1/3 z-50' alt="" />
+        <img src="/assets/rightarrow.png" className='absolute w-14 -right-64 lg:-right-1 top-1/3 z-10' alt="" />
       </div>
     </div>
   );
---
+++ app/(homepage)/GidaMOT.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const GidaMOT = () => {
+  return (
+    <div>
+      <div>
+        <h1>GIDA MOT league</h1>
+        <p>GIDA's “Masters of Trading League‶ - MOT League 💹 is a monthly subscription based premium and exclusive community for people actively involved in Trading the Crypto Spot and Futures Market as well as the Forex Market.</p>
+        <p>You get;</p>
+        <div>
+            <img src="/assets/checkIcon.png" alt="" />
+            <p>Access to Mentorship & Accountability partners to become a consistent & profitable trader</p>
+        </div>
+      </div>
+      <div>
+        <div>
+            <img src="/assets/MOT.png" alt="" />
+            <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GidaMOT
