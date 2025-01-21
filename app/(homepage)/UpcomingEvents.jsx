c9fdcc745750d827fd8afc5a0f3ce79bf460fba5
wheval <whevalmezeegbe@gmail.com>
2025-01-21 09:45:09 +0100

    add upcomingevents

---
+++ app/(homepage)/UpcomingEvents.jsx
@@ -0,0 +1,20 @@
+
+const UpcomingEvents = () => {
+  return (
+    <div className="px-8 py-12 lg:p-16">
+        <div className="max-w-screen-2xl mx-auto">
+            <div>
+                <h3>
+                    Upcoming Events
+                </h3>
+                <p>See all our events tailored to help you accelerate faster in your tech journey</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UpcomingEvents
