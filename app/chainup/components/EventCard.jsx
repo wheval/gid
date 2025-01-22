c01422ce61d39a3f5eaeccd9e85a92410ded4aeb
Femi Ojetokun <ojetokunakinwale@gmail.com>
2025-01-22 06:35:24 +0100

    add layout for chainup page events

---
+++ app/chainup/components/EventCard.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import Image from 'next/image';
+import { FaSearchLocation } from 'react-icons/fa';
+import TimeIcon from '../../../public/assets/timeIcon.svg';
+import LocationIcon from '../../../public/assets/locationIcon.svg';
+
+const EventCard = ({ event }) => {
+    return (
+        <div className="flex flex-col lg:flex-row items-center bg-[#FAF9F6] rounded-lg p-6 max-w-5xl mx-auto my-6 space-y-6 lg:space-y-0 lg:space-x-8">
+
+            {/* Date and Day */}
+            <div className="flex flex-col items-center lg:items-start text-center lg:text-left">
+                <span className="text-gray-700 text-lg font-semibold">{event.date}</span>
+                <span className="text-gray-500 text-md">{event.day}</span>
+                <div className="w-2 h-2 bg-gray-600 rounded-full mt-2 hidden lg:block"></div>
+            </div>
+
+            {/* Event Details Card */}
+            <div className="flex flex-col md:flex-row rounded-lg p-6 w-full">
+                <div className="text">
+                    <h2 className="text-gray-500 font-light text-lg">{event.time}</h2>
+                    <h3 className="text-black font-normal text-2xl mt-1">{event.title}</h3>
+
+                    {/* Countdown Timer */}
+                    <div className="flex items-center mb-4">
+                        <Image
+                            src={TimeIcon}
+                            alt="clock-icon"
+                            width={'10px'}
+                            height={'10px'}
+                            className="p-2"
+                        />
+                        <div className="flex justify-start my-4 space-x-6 text-center">
+                            <div>
+                                <span className="block font-light text-gray-700">Days</span>
+                                <span className="block font-normal text-black">2</span>
+                            </div>
+                            <div>
+                                <span className="block font-light text-gray-700">Hours</span>
+                                <span className="block font-normal text-black">12</span>
+                            </div>
+                            <div>
+                                <span className="block font-light text-gray-700">Minutes</span>
+                                <span className="block font-normal text-black">2</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Event Location */}
+                    <div className="flex items-center mb-4">
+                        <Image
+                            src={LocationIcon}
+                            alt="location-icon"
+                            width={'10px'}
+                            height={'10px'}
+                            className="p-2"
+                        />
+                        <span className="text-gray-600">{event.location}</span>
+                    </div>
+
+                    {/* Access Info */}
+                    <div className="flex items-center">
+                        <span className="bg-green-200 text-green-700 px-2 rounded-full font-normal">
+                            Free Access
+                        </span>
+                        <span className="text-gray-500 pl-2"> | </span>
+                        <span className="pl-3">{event.registered} Registered</span>
+                    </div>
+
+                    <button className="bg-red-700 w-50 text-white font-bold px-6 py-3 rounded-full mt-4 hover:bg-red-800 transition">
+                        Register For Event
+                    </button>
+                </div>
+
+                {/* Event Images */}
+                <div className='images md:ml-auto'>
+                    <div className="flex flex-col m-auto md:ml-auto lg:flex-row items-center justify-center lg:space-x-4 h-full">
+                        <div className="relative w-72 h-48 lg:w-56 lg:h-36 flex items-center">
+                            <Image
+                                src={event.image}
+                                alt="Event banner"
+                                layout="fill"
+                                objectFit="cover"
+                                className="p-2"
+                            />
+                        </div>
+
+                        <div className="relative w-72 h-48 lg:w-56 lg:h-36 flex items-center">
+                            <Image
+                                src={event.loc}
+                                alt="Event location"
+                                layout="fill"
+                                objectFit="cover"
+                                className="p-2"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default EventCard;
--- app/chainup/components/EventsSchedule.jsx
+++ app/chainup/components/EventsSchedule.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from 'react';
+import EventCard from './EventCard';
+
+import KadunaImg from '../../../public/assets/chain-up-kaduna.png';
+import KADImg from '../../../public/assets/kad.png';
+import IbadanImg from '../../../public/assets/chain-up-ibadan.png';
+import IBImg from '../../../public/assets/ib.png';
+import BeninImg from '../../../public/assets/chain-up-benin.png';
+import BNIImg from '../../../public/assets/bni.png';
+import AnambraImg from '../../../public/assets/chain-up-anambra.png';
+import ANImg from '../../../public/assets/an.png';
+import EnuguImg from '../../../public/assets/chain-up-enugu.png';
+import ENUImg from '../../../public/assets/enu.png';
 
 const EventsSchedule = () => {
-  return (
-    <div>EventsSchedule</div>
-  )
-}
+  const eventsScheduleData = [
+    {
+      date: "January 25, 2025",
+      day: "Saturday",
+      time: "10:00 AM",
+      title: "CHAIN UP KADUNA",
+      location: "The Budd, 19 Algeria Crescent, Barnawa, Kaduna",
+      registered: 230,
+      image: KadunaImg,
+      loc: KADImg,
+    },
+    {
+      date: "January 29, 2025",
+      day: "Wednesday",
+      time: "10:00 AM",
+      title: "CHAIN UP IBADAN",
+      location: "Seminar Room, ICC, University Of Ibadan Second Gate, Ibadan",
+      registered: 230,
+      image: IbadanImg,
+      loc: IBImg,
+    },
+    {
+      date: "February 01, 2025",
+      day: "Saturday",
+      time: "10:00 AM",
+      title: "CHAIN UP BENIN",
+      location: "Opolo Innovation Hub, UNIBEN, Benin City, Edo State",
+      registered: 230,
+      image: BeninImg,
+      loc: BNIImg,
+    },
+    {
+      date: "February 8, 2025",
+      day: "Wednesday",
+      time: "10:00 AM",
+      title: "CHAIN UP ANAMBRA",
+      location:
+        "Nawé Schools, Fab Amich House, 89 Club Road, Regina, Opposite Obalaku/Awka, Anambra State",
+      registered: 230,
+      image: AnambraImg,
+      loc: ANImg,
+    },
+    {
+      date: "February 15, 2025",
+      day: "Tuesday",
+      time: "10:00 AM",
+      title: "CHAIN UP ENUGU",
+      location:
+        "Lion Science Park, Off Sullivan Chime Drive, Greenhouse Gate, UNN Campus, Enugu State",
+      registered: 230,
+      image: EnuguImg,
+      loc: ENUImg,
+    },
+  ];
 
-export default EventsSchedule
+  return (
+    <div className="schedule-container bg-[#F0F0F0]">
+      <h1 className="schedule-title">Events Schedule</h1>
+
+      <div className="events-list">
+        {eventsScheduleData.map((event, index) => (
+          <EventCard key={index} event={event} />
+        ))}
+
+      </div>
+    </div>
+  )
+};
+
+export default EventsSchedule;
