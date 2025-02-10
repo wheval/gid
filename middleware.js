01cc76dbff23a7684d9f910e544cacc92a04bdd6
wheval <whevalmezeegbe@gmail.com>
2025-02-10 23:02:29 +0100

    add middleware

---
+++ middleware.js
@@ -0,0 +1,14 @@
+export function middleware(request) {
+    const { pathname } = request.nextUrl
+  
+    // Convert the pathname to lowercase if it contains uppercase letters
+    if (pathname !== pathname.toLowerCase()) {
+      return Response.redirect(
+        new URL(pathname.toLowerCase(), request.url)
+      )
+    }
+  }
+  
+  export const config = {
+    matcher: '/:path*'
+  }
