f19f2d2ae2f77bbf35716786163773ae0878c39c
wheval <whevalmezeegbe@gmail.com>
2025-02-11 17:42:36 +0100

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,14 +1,25 @@
 export function middleware(request) {
-    const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
   
-    // Convert the pathname to lowercase if it contains uppercase letters
-    if (pathname !== pathname.toLowerCase()) {
-      return Response.redirect(
-        new URL(pathname.toLowerCase(), request.url)
-      )
-    }
-  };
-  
+  // Skip for static files, images, and Vercel paths
+  if (
+    pathname.startsWith('/_vercel') ||
+    pathname.includes('/_next/') ||
+    pathname.match(/\.(jpg|jpeg|png|gif|svg|ico|webp)$/)
+  ) {
+    return;
+  }
+
+  // Convert the pathname to lowercase if it contains uppercase letters
+  if (pathname !== pathname.toLowerCase()) {
+    return Response.redirect(
+      new URL(pathname.toLowerCase(), request.url)
+    )
+  }
+};
+
 export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico|images/).*)'
+  matcher: [
+    '/((?!api|_next/static|_next/image|_vercel|favicon.ico|public/|assets/|images/).*)'
+  ]
 };
